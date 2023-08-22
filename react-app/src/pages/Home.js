43affa19f606a299435fe72b87a31d3c0f49f880
Michael Monson <michaelmonson@gmail.com>
2023-08-22 16:08:12 -0600

    Added Routing Functionality for page navigation    * Initial Routing Attempt - React is new to me!         -I have my pages defined, although empty at this point.         - Throwing a compile error in index.js         - Identifier 'App' has already been declared.         - Possibly because I've declared my routes inline in index.js, whereas 'App' has already been declared in a separate file.         - Pushing this out before experimenting.    * Continuing work on initial routing    * Cleaned up my index.js and App.js files         - I was uncertain if I needed both files... perhaps consolidate them?         - Researched: having separate index.js and App.js files is the recommended approach.         - Each file serves a specific purpose in the React App's entry and component hierarchy.         - index.js : entry point of my React App         - App.js : The main component of my App.
    Removed Routes from my index.js. Removed quite a bit, actually. Down to mostly the required imports and the placeholder for rendering my App.
    
    Moved a lot of the functionality that I had originally placed in my index file and moved it into my App.js file. This defines the structure and the paths.
    
    Added react-router-dom to my package.json configuration.
    
    Expanded my README file with a project structure section. Added development notes and such.
    
       * Successful in getting routing functional!
            - Final clean-up for Routing functionality
            - Adding additional menu links
            - Collapsing unordered-list links for readability
            - Eliminated unnecessary commented code from index.js
            - Updated README with Routing marked as complete.
    
       * Commenting out code within render method
            - not much in there... perhaps the tutorial was expecting more?
            - Commenting out, just in case.
            - Although I am new to React, it seems like that is a pretty standard approach, and required to run my App!
            - Perhaps similar code will replace it eventually?
       * Updated my time tracking notes.

---
+++ react-app/src/pages/Home.js
@@ -0,0 +1,10 @@
+import React from 'react';
+  
+function Home (){
+    return <div>
+             <h1>Welcome to the landing page of Jokes!</h1>
+             <span>If you see this message, it means routing was successful!</span>
+           </div>
+}
+  
+export default Home;
---
+++ react-app/src/pages/Jokes.js
@@ -0,0 +1,8 @@
+import React from 'react';
+  
+function Jokes () {
+    return <div>
+        <h2>Chuck Norris Jokes!</h2>
+    </div>
+}
+export default Jokes;
